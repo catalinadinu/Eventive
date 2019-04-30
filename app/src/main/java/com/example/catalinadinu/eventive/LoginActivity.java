dba088bf4260eb4f610aaef813ada21fcbd0e24b
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2019-04-30 18:50:57 +0300

    Comunicare Login -> Signup

--- app/src/main/java/com/example/catalinadinu/eventive/LoginActivity.java
+++ app/src/main/java/com/example/catalinadinu/eventive/LoginActivity.java
@@ -3,27 +3,34 @@ package com.example.catalinadinu.eventive;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-    private CardView cardConectare;
     private TextView skipConectare;
+    private TextView inregistrare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        cardConectare = findViewById(R.id.login_conectare);
         skipConectare = findViewById(R.id.login_skipConectare);
+        inregistrare = findViewById(R.id.login_contNou);
 
         skipConectare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent dashboardIntent = new Intent(LoginActivity.this, DashboardActivity.class);
                 startActivity(dashboardIntent);
+            }
+        });
+
+        inregistrare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inregistrareIntent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(inregistrareIntent);
             }
         });
     }
--- app/src/main/java/com/example/catalinadinu/eventive/SignupActivity.java
+++ app/src/main/java/com/example/catalinadinu/eventive/SignupActivity.java
@@ -1,13 +1,26 @@
 package com.example.catalinadinu.eventive;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignupActivity extends AppCompatActivity {
+    private TextView conectare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        conectare=findViewById(R.id.signup_conectare);
+        conectare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
