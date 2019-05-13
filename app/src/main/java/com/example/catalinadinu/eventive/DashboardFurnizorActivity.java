8fe23107583346c4188864cc6a4f33d800441051
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2019-05-13 03:49:53 +0300

    UI cont furnizor
    Vezi ca inca incurca borcanele cu userii - cred ca ramane tokenul userului curent salvat si la urmatoarea conectare ma trimite pe tot pe acelasi tip de user ca cel logat initial

--- app/src/main/java/com/example/catalinadinu/eventive/DashboardFurnizorActivity.java
+++ app/src/main/java/com/example/catalinadinu/eventive/DashboardFurnizorActivity.java
@@ -1,13 +1,73 @@
 package com.example.catalinadinu.eventive;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DashboardFurnizorActivity extends AppCompatActivity {
+
+    private CardView serviciileMele, setari, info, deconectare;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_furnizor);
+
+        serviciileMele = findViewById(R.id.furnizor_listaServicii);
+        setari = findViewById(R.id.furnizor_setari);
+        info = findViewById(R.id.furnizor_info);
+        deconectare = findViewById(R.id.furnizor_deconectare);
+
+
+
+        deconectare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intentDeconectare = new Intent(DashboardFurnizorActivity.this, LoginActivity.class);
+                intentDeconectare.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intentDeconectare);
+            }
+        });
+
+        setupFirebaseListener();
+    }
+
+    private void setupFirebaseListener(){
+        Log.d("Tag",  "setting up the auth state listener");
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Log.d("Tag", "onAuthStateChanged: signed_in" + user.getUid());
+
+                }else{
+                    Log.d("Tag", "onAuthStateChanged: signed_out" + user.getUid());
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(authStateListener != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(authStateListener);
+        }
     }
 }
