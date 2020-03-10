7178a2dcf26f81bf0a19a96fba81a1b61dc2e7d6
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2020-03-10 13:20:37 +0200

    Redenumire activitati

--- app/src/main/java/com/example/catalinadinu/eventive/ProfilContFurnizorActivity.java
+++ app/src/main/java/com/example/catalinadinu/eventive/ProfilContFurnizorActivity.java
@@ -1,40 +1,59 @@
 package com.example.catalinadinu.eventive;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.example.catalinadinu.eventive.Clase.Const;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class DashboardFurnizorActivity extends AppCompatActivity {
+public class ProfilContFurnizorActivity extends AppCompatActivity {
 
     private CardView serviciileMele, setari, info, deconectare;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private SharedPreferences sp;
+    private TextView textWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_furnizor);
+        setContentView(R.layout.activity_profil_cont_furnizor);
 
         serviciileMele = findViewById(R.id.furnizor_listaServicii);
         setari = findViewById(R.id.furnizor_setari);
         info = findViewById(R.id.furnizor_info);
         deconectare = findViewById(R.id.furnizor_deconectare);
 
+        textWelcome = findViewById(R.id.cont_textWelcomeClient);
 
+        String stringWelcome = "Bun venit in cont, " + FirebaseAuth.getInstance().getCurrentUser().getEmail() + "! :)";
+
+        textWelcome.setText(stringWelcome);
 
         deconectare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sp = getSharedPreferences(Const.SP_FILE_NAME, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.remove(Const.SP_EMAIL_KEY);
+                editor.remove(Const.SP_PASSWORD_KEY);
+                editor.apply();
+
                 FirebaseAuth.getInstance().signOut();
-                Intent intentDeconectare = new Intent(DashboardFurnizorActivity.this, LoginActivity.class);
+
+                Intent intentDeconectare = new Intent(ProfilContFurnizorActivity.this, LoginActivity.class);
                 intentDeconectare.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intentDeconectare);
+                finish();
+
             }
         });
 
