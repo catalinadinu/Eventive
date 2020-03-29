831ab47002913c7fe6da52700d323982aa8c0846
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2020-03-29 21:24:58 +0300

    Layout detalii cont furnizor
    (formular introducere date)

--- app/src/main/java/com/example/catalinadinu/eventive/ContFurnizorActivity.java
+++ app/src/main/java/com/example/catalinadinu/eventive/ContFurnizorActivity.java
@@ -32,6 +32,14 @@ public class ContFurnizorActivity extends AppCompatActivity {
         String stringWelcome = "Bun venit in cont, " + FirebaseAuth.getInstance().getCurrentUser().getEmail() + "! :)";
         textWelcome.setText(stringWelcome);
 
+        setari.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContFurnizorActivity.this, SetariContFurnizorActivity.class);
+                startActivity(intent);
+            }
+        });
+
         deconectare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/example/catalinadinu/eventive/SetariContFurnizorActivity.java
@@ -0,0 +1,13 @@
+package com.example.catalinadinu.eventive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetariContFurnizorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setari_cont_furnizor);
+    }
+}
