021904a9c5e55baf40545c2fe5dd033a607c57e2
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2020-04-12 20:58:00 +0300

    Trimitere mail la adresa prestabilita

--- app/src/main/java/com/example/catalinadinu/eventive/ContactAppActivity.java
+++ app/src/main/java/com/example/catalinadinu/eventive/ContactAppActivity.java
@@ -1,13 +1,49 @@
 package com.example.catalinadinu.eventive;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class ContactAppActivity extends AppCompatActivity{
+    private TextView sendMail;
+    private TextView visitSite;
+    private TextView apelTelefon;
+    private RatingBar ratingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_app);
+
+        initComponents();
+
+        visitSite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://pdm.ase.ro/"));
+                startActivity(intent);
+            }
+        });
+
+        sendMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sendEmail = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"+"help.eventive@gmail.com"));
+                startActivity(Intent.createChooser(sendEmail, "Choose an email client from..."));
+            }
+        });
     }
+
+    private void initComponents(){
+        visitSite = findViewById(R.id.contact_site);
+        sendMail = findViewById(R.id.contact_app_mail);
+        apelTelefon = findViewById(R.id.contact_app_phone);
+        ratingBar = findViewById(R.id.contact_app_rating_bar);
+    }
+
+
 }
