c141d16d74b4cfb7dabaa34c07a33234eb7a7101
Catalina Mihaela Dinu <mihaeladinu97@gmail.com>
2020-07-03 20:57:35 +0300

    Fixare bug imagine in activitatea de vizualizare serviciu

--- app/src/main/java/com/example/catalinadinu/eventive/VizualizareServiciuActivity.java
+++ app/src/main/java/com/example/catalinadinu/eventive/VizualizareServiciuActivity.java
@@ -206,7 +206,7 @@ public class VizualizareServiciuActivity extends AppCompatActivity {
     }
 
     private void getImageFromFirebaseStorage(){
-        String photoName = serviciuDeAfisat.getNumeFurnizor() + "/" + serviciuDeAfisat.getCategorie() + "/" + serviciuDeAfisat.getDenumire();
+        String photoName = serviciuDeAfisat.getMailUtilizator() + "/" + serviciuDeAfisat.getCategorie() + "/" + serviciuDeAfisat.getDenumire();
         StorageReference ref = storageReference.child(photoName);
         try{
             final File file = File.createTempFile("image", "jpg");
